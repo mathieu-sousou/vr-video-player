b67f49aac5faa308f5a4620c3df02d25d7175fb1
Mathieu Souchaud <mathieu.souchaud@surgar-surgery.com>
2024-10-14 23:38:59 +0200

    add libxdo deps directly because not found on ubuntu22.04

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -e
 
-dependencies="glew sdl2 openvr x11 xcomposite xfixes mpv libxdo"
+set -x
+
+dependencies="glew sdl2 openvr x11 xcomposite xfixes mpv"
+#libxdo"
 includes=$(pkg-config --cflags $dependencies)
 libs="$(pkg-config --libs $dependencies) -lm -pthread"
 gcc -c src/window_texture.c -O2 -DNDEBUG $includes
 g++ -c src/mpv.cpp -O2 -DNDEBUG $includes
 g++ -c src/main.cpp -O2 -DNDEBUG $includes
-g++ -o vr-video-player -O2 window_texture.o mpv.o main.o -s $libs
+g++ -o vr-video-player -O2 window_texture.o mpv.o main.o -s -lxdo $libs
